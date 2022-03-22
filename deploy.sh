51a894508fba2fc543cf2835028045eda1333a87
Ivan0719 <m24938142@gmail.com>
2022-03-22 12:32:37 +0000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt-get install firefox -y
+sudo apt-get install firefox python3 python3-pip -y
+python3 -m pip install --upgrade pip
 pip3 install requests
 pip3 install bs4
 pip3 install selenium
